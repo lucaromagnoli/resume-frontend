989a786b8627beb94f38b692189451858a2c8e7a
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-12 15:08:48 +0000

    chnage url for prod

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,12 +1,18 @@
-import {Experience} from "./experience";
 import axios, {AxiosResponse} from "axios";
 
-const url = "api/experiences"
-
-const config = {
-  url
+function getURL(): string {
+  let url;
+  if (import.meta.env.DEV) {
+    url = "api/experiences";
+  }
+  else {
+    url = "https://lr-resume.herokuapp.com/api/experiences";
+  }
+  return url
 }
+
 async function fetchExperiences(): Promise<AxiosResponse> {
+  const url = getURL();
   return axios(url);
 }
 
