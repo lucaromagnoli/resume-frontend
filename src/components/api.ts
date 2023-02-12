4e235344ff7df8cef437376b4d136523b5aeec0a
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-12 22:36:09 +0000

    update redirects and hardcode url

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,18 +1,18 @@
 import axios, { AxiosResponse } from "axios";
 
-function getURL(): string {
-  let url;
-  if (import.meta.env.DEV) {
-    url = "api/experiences";
-  } else {
-    url = "https://lr-resume.herokuapp.com/api/experiences";
-  }
-  return url;
-}
+// function getURL(): string {
+//   let url;
+//   if (import.meta.env.DEV) {
+//     url = "api/experiences";
+//   } else {
+//     url = "https://lr-resume.herokuapp.com/api/experiences";
+//   }
+//   return url;
+// }
 
 async function fetchExperiences(): Promise<AxiosResponse> {
-  const url = getURL();
-  return axios(url);
+  // const url = getURL();
+  return axios("experiences");
 }
 
 export default fetchExperiences;
