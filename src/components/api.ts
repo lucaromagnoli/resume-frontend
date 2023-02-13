fa427d62a930f40a5eee710eaea2c9fea88e1743
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-13 11:24:02 +0000

    remove unused function

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,15 +1,5 @@
 import axios, { AxiosResponse } from "axios";
 
-// function getURL(): string {
-//   let url;
-//   if (import.meta.env.DEV) {
-//     url = "api/experiences";
-//   } else {
-//     url = "https://lr-resume.herokuapp.com/api/experiences";
-//   }
-//   return url;
-// }
-
 async function fetchExperiences(): Promise<AxiosResponse> {
   // const url = getURL();
   return axios("api/experiences/");
